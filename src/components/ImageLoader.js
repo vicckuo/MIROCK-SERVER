f9ebe552c02cf961bce8dbebc06ed2c59dd33907
Vic Kuo <eun.hru@gmail.com>
2024-05-12 20:50:52 +0800

    add: bg-image repeat

--- src/components/ImageLoader.js
+++ src/components/ImageLoader.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-import Logo from '../assets/images/logo.png';
-import SocialMedia from './SocialMedia';
+import Logo from "../assets/images/logo.png";
+import SocialMedia from "./SocialMedia";
 
-import Countdown from 'react-countdown';
+import Countdown from "react-countdown";
 
 function ImageLoader() {
   const { imageId } = useParams();
 
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [expired, setExpired] = useState(false); // 新增状态用于跟踪图片是否过期
@@ -30,10 +30,12 @@ function ImageLoader() {
 
     const checkImageExpiration = async () => {
       try {
-        const response = await fetch(imageUrl, { method: 'HEAD' });
-        const lastModified = response.headers.get('last-modified');
+        const response = await fetch(imageUrl, { method: "HEAD" });
+        const lastModified = response.headers.get("last-modified");
         const lastModifiedDate = new Date(lastModified);
-        const newExpirationDate = new Date(lastModifiedDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+        const newExpirationDate = new Date(
+          lastModifiedDate.getTime() + 7 * 24 * 60 * 60 * 1000
+        );
 
         setExpirationDate(newExpirationDate);
 
@@ -43,7 +45,7 @@ function ImageLoader() {
           setExpired(false);
         }
       } catch (error) {
-        console.error('Error checking image expiration:', error);
+        console.error("Error checking image expiration:", error);
         setError(true);
       } finally {
         setLoading(false);
@@ -56,11 +58,11 @@ function ImageLoader() {
   const countdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
       // 时间已到，显示过期信息
-      return <div className='text-white my-4'>可下載時間已過期，謝謝。</div>;
+      return <div className="text-white my-4">可下載時間已過期，謝謝。</div>;
     } else {
       // 时间未到，显示剩余时间
       return (
-        <div className='text-white my-2'>
+        <div className="text-white my-2">
           照片剩餘時間：{days}天 {hours}時 {minutes}分 {seconds}秒
         </div>
       );
@@ -78,74 +80,85 @@ function ImageLoader() {
   };
 
   return (
-    <div className={`flex flex-col items-center lg:justify-center h-screen bg-black`}>
+    <div className="flex flex-col items-center lg:justify-center h-screen bg-black relative overflow-hidden">
+      <div
+        className="absolute inset-0 opacity-75"
+        style={{
+          backgroundImage: `url('/mirock.png')`,
+          backgroundSize: "10%",
+          transform: "rotate(-45deg) scale(2.5)",
+          backgroundRepeat: "space",
+        }}
+      ></div>
+
       {loading && (
-        <div
-          id='loadingMessage'
-          className='text-lg text-white m-auto'>
+        <div id="loadingMessage" className="text-lg text-white m-auto z-10">
           圖片讀取中，請稍後...
         </div>
       )}
       {!loading && !error && expirationDate && (
-        <>
+        <React.Fragment className="z-10">
           <img
             src={imageUrl}
-            alt='Images'
-            className='object-contain w-full h-1/2 pt-8'
-            style={{ display: !expired ? 'block' : 'none' }}
+            alt="Images"
+            className="object-contain w-full h-1/2 pt-8"
+            style={{ display: !expired ? "block" : "none" }}
           />
-          <Countdown
-            date={expirationDate}
-            renderer={countdownRenderer}
-          />
-          <div
-            id='downloadButton'
-            className='mt-4 relative'>
+          <Countdown date={expirationDate} renderer={countdownRenderer} />
+          <div id="downloadButton" className="mt-4 relative">
             <button
               onClick={showSaveImageTip}
               style={{
-                display: !expired ? 'block' : 'none',
-                background: 'linear-gradient(45deg, #bf953f, #fcf6ba, #b38728, #fbf5b7, #aa771c)',
+                display: !expired ? "block" : "none",
+                background:
+                  "linear-gradient(45deg, #bf953f, #fcf6ba, #b38728, #fbf5b7, #aa771c)",
               }}
-              className='px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-700 transition duration-300 flex items-center justify-center m-auto'>
+              className="px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-700 transition duration-300 flex items-center justify-center m-auto"
+            >
               怎麼保存圖片？
             </button>
 
-            <div className='text-3xl text-highlight-light z-10 text-white flex flex-col items-center justify-center m-auto'>
+            <div className="text-3xl text-highlight-light z-10 text-white flex flex-col items-center justify-center m-auto">
               <img
                 src={Logo}
-                alt='logo'
-                className='w-40 h-40 lg:w-48 lg:h-48 overflow-hidden cursor-pointer'
-                onClick={() => (window.location.href = 'https://www.easy4music.com/mirock')}
+                alt="logo"
+                className="w-40 h-40 lg:w-48 lg:h-48 overflow-hidden cursor-pointer"
+                onClick={() =>
+                  (window.location.href = "https://www.easy4music.com/mirock")
+                }
               />
               <SocialMedia />
             </div>
           </div>
-        </>
+        </React.Fragment>
       )}
       {!loading && error && (
-        <div className='text-lg text-white flex flex-col items-center justify-center m-auto'>
+        <div className="text-lg text-white flex flex-col items-center justify-center m-auto z-10">
           圖片讀取失敗，請稍後再試。
           <button
             style={{
-              background: 'linear-gradient(45deg, #bf953f, #fcf6ba, #b38728, #fbf5b7, #aa771c)',
+              background:
+                "linear-gradient(45deg, #bf953f, #fcf6ba, #b38728, #fbf5b7, #aa771c)",
             }}
             onClick={() => window.location.reload()}
-            className='px-4 py-2 mt-4 bg-blue-500 text-black rounded hover:bg-blue-700 transition duration-300'>
+            className="px-4 py-2 mt-4 bg-blue-500 text-black rounded hover:bg-blue-700 transition duration-300"
+          >
             重新整理
           </button>
         </div>
       )}
       {showTip && (
-        <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-10'>
-          <div className='bg-white p-4 rounded-lg shadow-lg text-center'>
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-10">
+          <div className="bg-white p-4 rounded-lg shadow-lg text-center">
             <p>長按圖片，然後選擇“儲存到照片”，就會存到手機相簿囉！</p>
             <button
               style={{
-                background: 'linear-gradient(45deg, #bf953f, #fcf6ba, #b38728, #fbf5b7, #aa771c)',
+                background:
+                  "linear-gradient(45deg, #bf953f, #fcf6ba, #b38728, #fbf5b7, #aa771c)",
               }}
-              className='mt-4 px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-700 transition duration-300'
-              onClick={closeTip}>
+              className="mt-4 px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-700 transition duration-300"
+              onClick={closeTip}
+            >
               OK
             </button>
           </div>
